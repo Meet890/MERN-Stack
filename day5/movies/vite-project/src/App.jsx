e2737f06b79c7e0a6ad33b74a50b83c562804059
Meet Rathod <139968514+Meet890@users.noreply.github.com>
2025-07-09 15:38:38 +0530

    movies api , useMemo method

--- day5/movies/vite-project/src/App.jsx
+++ day5/movies/vite-project/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Movies from './component/Movies'
+import Movie from './component/Movie'
+import UseMemo from './component/UseMemo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Movies/>
+      {/* <Movie /> */}
+      <UseMemo/>
     </>
   )
 }
---
+++ day5/movies/vite-project/src/component/UseMemo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UseMemo() {
+  return (
+    <div>UseMemo
+
+        
+    </div>
+  )
+}
+
+export default UseMemo
